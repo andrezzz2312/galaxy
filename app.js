56400a4e9ae678be262cb5410527c97e72d73814
Andrez Ramirez <andrezzz2312@gmail.com>
2022-08-05 08:18:04 -0500

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { request } = require('http')
 const app = express()
 const path = require('path')
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/src'))
 app.use(
 	'/build/',
 	express.static(path.join(__dirname, 'node_modules/three/build'))
